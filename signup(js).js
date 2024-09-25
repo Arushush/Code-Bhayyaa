09c3a9e5aac398f0e6ed66e5509284ea5eb41889
Arushush <135645508+Arushush@users.noreply.github.com>
2024-09-26 03:56:42 +0530

    Add files via upload

---
+++ signup(js).js
@@ -0,0 +1,33 @@
+document.getElementById('signupForm').addEventListener('submit', async (e) => {
+    e.preventDefault(); // Prevent the form from submitting the default way
+
+    // Get email and password from the form
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    console.log(email)
+    console.log(password)
+    try {
+        const response = await fetch('http://127.0.0.1:5000/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                'email': email,
+                'password': password,
+            })
+        });
+
+        const result = await response.json();
+        if (response.ok && result.status !==false) {
+            // Handle success (e.g., navigate to another page or show success message)
+            alert(result.message);
+            console.log(result.uid); // User's UID
+        } else if (result.status == false){
+            // Handle error (e.g., show error message)
+            alert(result.error);
+        }
+    } catch (error) {
+        console.error('Error signing up:', error);
+    }
+});
